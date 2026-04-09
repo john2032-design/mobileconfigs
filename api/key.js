dd33c883f50636ef90dc0cc2bac3344a366bdef7
John <faysal.heshmatullah@gmail.com>
2026-04-08 21:55:23 -0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -38,6 +38,11 @@ async function ensureTable() {
 }
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') return res.status(200).end()
+
   try {
     await ensureTable()
   } catch (err) {
